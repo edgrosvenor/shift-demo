f798c992f4314e6568827e6a00cc58bef689e492
Shift <shift@laravelshift.com>
2022-11-08 18:57:38 +0000

    Apply Laravel coding style
    Shift automatically applies the Laravel coding style - which uses the PSR-12 coding style as a base with some minor additions.
    
    You may customize the code style applied by configuring [Pint](https://laravel.com/docs/pint), [PHP CS Fixer](https://github.com/FriendsOfPHP/PHP-CS-Fixer), or [PHP CodeSniffer](https://github.com/squizlabs/PHP_CodeSniffer) for your project root.
    
    For more information on customizing the code style applied by Shift, [watch this short video](https://laravelshift.com/videos/shift-code-style).

--- app/Http/Controllers/AboutMeController.php
+++ app/Http/Controllers/AboutMeController.php
@@ -2,21 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class AboutMeController extends Controller
 {
     public function __invoke()
     {
         // TODO: Have Shift turn this into a more modern looking array
-        $about = array(
+        $about = [
             'My name is Ed Grosvenor',
             'I am the Director of Application Development at EXACT Sports in Chicago, Illinois',
             'I grew up in Chicago and moved to Basel in August with my wife and two sons.',
             'I have been a PHP developer since version 3 and did lots of Perl before that.',
             'My email address is ed@gros.co',
             'My Twitter handle is @MaybeEdward',
-        );
+        ];
 
         return view('about-me', ['about' => $about]);
     }
